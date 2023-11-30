edb23a52e31ec7a1093e25b7aaf513f055fa3dde
Rishvanth-Kumaresan <102664560+Rishvanth-Kumaresan@users.noreply.github.com>
2023-11-30 14:28:08 +0530

    Added sample.

---
+++ SaveEdits_BackButton/SaveEdits_BackButton/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace SaveEdits_BackButton;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new MainPage();
+	}
+}
---
+++ SaveEdits_BackButton/SaveEdits_BackButton/MainPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using Syncfusion.Maui.ImageEditor;
+
+namespace SaveEdits_BackButton;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+		this.imageEditor.ToolbarItemSelected += OnToolbarItemSelected;
+
+    }
+
+    private void OnToolbarItemSelected(object sender, ToolbarItemSelectedEventArgs e)
+    {
+        if (e.ToolbarItem != null && e.ToolbarItem.Name == "Back")
+        {
+            imageEditor.SaveEdits();
+        }
+    }
+}
